c98e06e67cd067e0987a8b8114e6d2f6d020dc42
Touir <dima.minchenkov.88@mail.ru>
2022-03-26 20:33:02 +0300

    changed code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-int main() {
-  char* name = "";//name variable initialisation
- std::cout << "Your name is: "; 
- std::cin >> name;//name reading
- std::cout << "Hello world from" << name << std::endl;//output
-  return 0;
+int
+main()
+{
+  char* name = "";//data initialisation
+  std::cout << "Your name is: ";
+  std::cin >> name;//name reading
+  std::cout << "Hello world from" << name << endl;//output
+  std::return 0;
 }
